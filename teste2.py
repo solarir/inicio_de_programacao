c140fffa3c758a60e6739e75b2a500c2a6a8f271
GBgustavodutra <gustavo.dutra@guiabolso.com>
2022-05-26 19:04:32 -0300

    Primeiro commit

---
+++ teste2.py
@@ -0,0 +1,13 @@
+x = 1 # Int
+y = 2.0 # Double ou float
+z = "teste" # Strings
+
+a = 5
+x+a
+b = x+a
+print(b)
+
+num_1 = input("Escreva o primeiro numero: ")
+num_2 = input("Escreva o segundo numero: ")
+
+print(num_1 + num_2)
