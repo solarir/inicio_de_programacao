b9e181f9aa8f6b9717fbd9c3b7817e975ff34065
GBgustavodutra <gustavo.dutra@guiabolso.com>
2022-05-26 19:50:18 -0300

    treino de comando

---
+++ operacoes.py
@@ -0,0 +1,37 @@
+def soma(n1, n2):  
+    # def - Comando para criar uma funcao
+    # *nome_na_frente_do_def* - nome da funcao
+    # variaveis no parenteses - argumentos da funcao
+    return n1 + n2 # return volta o resultado passado para ele
+
+def subitracao(n1, n2):
+    return n1 - n2
+
+def multiplicacao(n1, n2):
+    n3 = n1 * n2
+    return n3 
+
+def divisao(n1, n2):
+    n3 = float(n1) / float(n2)
+    return n3
+
+num_1 = input("Escreva o primeiro numero: ")
+num_2 = input("Escreva o segundo numero: ")
+
+resultado_1 = soma(n1 = num_1, n2 = num_2)
+resultado_2 = subitracao(n1 = num_1, n2 = num_2)
+resultado_3 = multiplicacao(n1 = num_1, n2 = num_2)
+resultado_4 = divisao(n1 = num_1, n2 = num_2)
+print("soma " + str(resultado_1))
+print("subitracao " + str(resultado_2))
+print("multiplicacao " + str(resultado_3))
+print("divisao " + str(resultado_4))
+
+# type cast : ato de transformar um tipo de variavel em outro. 
+# str: texto; float/doble: numero fracionado; int: numero inteiro.
+# juntar texto, listar ou objetos se chama concatenação
+# def = comando para criar função
+# print = "escrever na tela"
+# return = retornar ao comando ou variavel indicada.
+# input = entrada dinamica: se adapita ao tipo de variavel indicada.
+# output = print
--- teste2.py
+++ teste2.py
@@ -7,7 +7,18 @@ x+a
 b = x+a
 print(b)
 
+# I/O
+# inputs e outputs de programa
+
 num_1 = input("Escreva o primeiro numero: ")
 num_2 = input("Escreva o segundo numero: ")
 
 print(num_1 + num_2)
+
+print(6-4)
+
+print(8*3)
+
+print(3.0/4.0)
+
+print(3**7)
