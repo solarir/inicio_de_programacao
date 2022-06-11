c2761684cc1d78fc0fe0181ab5370df7ccffaff3
GBgustavodutra <gustavo.dutra@guiabolso.com>
2022-06-11 11:09:29 -0300

    Correção

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,6 @@
-def calculo():
+# Funções devem ser simplificadas para facilitar a leitura do código
+# Se a lógica pode sem encapsulada em uma função a mesma deve ser deslocada 
+def get_input_data():
     operacao = input('''Escolha uma das funcoes para realizar o seu calculo:
 + para soma
 - para subtracao
@@ -8,24 +10,42 @@ def calculo():
 
     num_1 = float(input("Escreva o primerio numero:"))
     num_2 = float(input("Escreva o segundo numero:"))
+    return[operacao, num_1, num_2]
 
-    if operacao == '+':
-        print('{} + {} = '.format(num_1, num_2))
-        print(num_1 + num_2)
+def sum(num_1, num_2): 
+    print('{} + {} = '.format(num_1, num_2))
+    print(num_1 + num_2)
+
+def subtraction(num_1, num_2):
+    print('{} - {} = '.format(num_1, num_2))
+    print(num_1 - num_2)
+
+def multiply(num_1, num_2):
+    print('{} * {} = '.format(num_1, num_2))
+    print(num_1 * num_2)
+    
+def division(num_1, num_2):
+    print('{} / {} = '.format(num_1, num_2))
+    print(num_1 / num_2)
+
+
+def calculate(operacao, num_1, num_2):
+    if operacao == '+': # Isso facilita a leitura nesse ponto
+        sum(num_1, num_2) # Fica laro que o retorno é uma soma 
 
     elif operacao == '-':
-        print('{} - {} = '.format(num_1, num_2))
-        print(num_1 - num_2)
+        subtraction(num_1, num_2)
 
     elif operacao == '*':
-        print('{} * {} = '.format(num_1, num_2))
-        print(num_1 * num_2)
+        multiply(num_1, num_2)
 
     elif operacao == '/':
-        print('{} / {} = '.format(num_1, num_2))
-        print(num_1 / num_2)
+        division(num_1, num_2)
 
     else:
         print("Operacao incorreta. Por facor, tente novamente.")
 
-calculo()
+# Se a funcao chama calculate ela deve conter apenas calculos
+# A funcao de input deve ter um nome que remete a entrada
+[operacao, num_1, num_2] = get_input_data()
+calculate(operacao, num_1, num_2) 
