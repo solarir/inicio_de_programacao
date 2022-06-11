8ff52a6f00b8736e4c400e3d54568807f4e1e62b
GBgustavodutra <gustavo.dutra@guiabolso.com>
2022-06-11 11:41:06 -0300

    Mais exemplos

--- exercicio_1/operacoes_em_lista.py
+++ exercicio_1/operacoes_em_lista.py
@@ -10,9 +10,15 @@ def find_similar_in_list(list, value_to_search):
     
     print("{value} appears {found} times in list".format(value=value_to_search, found=values_found))
 
-def remove_one_ocurrence_from_list(list, value_to_remove):
-    list.remove(value_to_remove) # Remove retira um valor igual ao passado da lista, caso exista
-    print("List without value {value} : {list}".format(value=value_to_remove, list=list))
+def list_of_ocurrences(list, value_to_find):
+    # Retorna todos valores iguais a value_to_find
+    list_of_desired_values = []
+    for value in list:
+        if value == value_to_find:
+            list_of_desired_values.append(value) # Adiciona item na lista
+
+    print(list_of_desired_values)
+
 
 def concatenate_all_values(list):
     complete_string = ""
@@ -22,12 +28,18 @@ def concatenate_all_values(list):
     print(complete_string)
 
 def sort_list(list):
-    print(list.sort())
+    sorted_list = sorted(list)
+    print(sorted_list)
+
+def invert_list(list):
+    inverted_list = list[::-1] # Isso representa como se fosse um for dentro da lista invertendo ela
+    print(inverted_list)
 
 list_of_strings = ["oi", "boi", "sol", "oi"]
 value_to_use = "oi"
 
 find_similar_in_list(list_of_strings, value_to_use)
-remove_one_ocurrence_from_list(list_of_strings, value_to_use)
+list_of_ocurrences(list_of_strings, value_to_use)
 concatenate_all_values(list_of_strings)
 sort_list(list_of_strings)
+invert_list(list_of_strings)
